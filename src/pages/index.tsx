3c80bd9b81ff5a58254ce073b113f4edd61f9fd7
Julianne <juliannelicon@gmail.com>
2021-07-22 21:03:36 -0300

    Adiciona campo search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import {
   Paper,
   Hidden,
   Button,
+  InputBase,
   Typography,
   TableContainer,
   Table,
@@ -10,16 +11,60 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  styled,
+  alpha,
 } from '@material-ui/core';
 
 import {
   Add as AddIcon,
-  // Search as SearchIcon,
+  Search as SearchIcon,
   Edit as EditIcon,
   Delete as DeleteIcon,
 } from '@material-ui/icons';
 
 import { useUsers } from '../context/UseUsersContext';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.black, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 export default function Home(): JSX.Element {
   const { users } = useUsers();
@@ -55,6 +100,16 @@ export default function Home(): JSX.Element {
             </Button>
           </Hidden>
         </Box>
+
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Pesquisar…"
+            inputProps={{ 'aria-label': 'pesquisar' }}
+          />
+        </Search>
 
         <TableContainer>
           <Table size="small" aria-label="a dense table">
